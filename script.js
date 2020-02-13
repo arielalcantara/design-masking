7bfa571dd968e01089249e28112d0724b3840861
Ariel Alcantara <ariel.a@digitalroominc.com>
2020-02-13 17:43:27 +0800

    Major changes: Supported white background
    WIP:
    Support for feathered designs

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let side = 'front';
 // Set image sources
 if (side == 'front') {
     artwork.src = "uploads/front-artwork.png";
-    text.src = "uploads/front-text.png";
+    text.src = "uploads/front-text-white.png";
 } else if (side == 'back') {
     artwork.src = "uploads/back-artwork.png";
     text.src = "uploads/back-text.png";
@@ -27,7 +27,6 @@ if (canvas.getContext) {
     // Add loader here or set canvas size to 0
     canvas.width = 0;
     canvas.height = 0;
-    console.log(canvas.width, canvas.height);
 
     artwork.onload = () => {
         text.onload = () => {
@@ -36,13 +35,141 @@ if (canvas.getContext) {
                 canvas.height = artwork.height;
                 canvas.width = artwork.width;
 
-                // Draw images
+                // Draw text image
                 ctx.drawImage(text, 0, 0);
+
+                // Process drawn image (white to alpha)
+                let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                imageData.data = normalizeImage(imageData.data);
+                ctx.putImageData(imageData, 0, 0);
                 ctx.globalCompositeOperation = 'source-in';
                 ctx.drawImage(texture, 0, 0);
+
+                let imageFeatheredData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                imageFeatheredData.data = getFeatheredAreas(imageData.data);
+
+                // Draw remaining images
+                ctx.globalCompositeOperation = 'source-in';
+                ctx.putImageData(imageFeatheredData, 0, 0);
                 ctx.globalCompositeOperation = 'destination-over';
                 ctx.drawImage(artwork, 0, 0);
             }
         }
     }
 }
+
+const uploadFrontBtn = document.getElementById('upload_front_btn');
+uploadFrontBtn.addEventListener('click', () => {
+    uploadImage('front');
+});
+
+const uploadBackBtn = document.getElementById('upload_back_btn');
+uploadBackBtn.addEventListener('click', () => {
+    uploadImage('back');
+});
+
+function uploadImage(side) {
+    if (side && side.length && side.length != 0) {
+        console.log(side);
+        return;
+    }
+}
+
+function normalizeImage(data) {
+    let dataCopy = data; // Create data copy for reference
+    console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+        if (!checkIfAlphaIndex(i + 1) || checkIfAlpha(data, i)) {
+            continue;
+        }
+
+        if (checkIfWhite(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1])) {
+            data = changeColorToAlpha(data, i);
+            continue;
+        }
+
+        // if (!checkIfBlack(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1] && calculateRGBStandardDeviation(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1]) < 25)) {
+        // if (calculateRGBStandardDeviation(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1]) < 3) {
+        //     data = changeColorToBlack(data, i);
+        //     data[i] = 255;
+        //     continue;
+        // }
+    }
+
+    console.log(data);
+    return data;
+}
+
+function getFeatheredAreas(data) { // modified func for getting non-B&W pixels
+    let dataCopy = data; // Create data copy for reference
+    console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+        if (!checkIfAlphaIndex(i + 1) || checkIfAlpha(data, i)) {
+            continue;
+        }
+
+        if (checkIfWhite(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1]) || checkIfBlack(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1])) {
+            data = changeColorToAlpha(data, i);
+            continue;
+        }
+
+        // if (!checkIfBlack(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1] && calculateRGBStandardDeviation(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1]) < 25)) {
+        // if (calculateRGBStandardDeviation(dataCopy[i - 3], dataCopy[i - 2], dataCopy[i - 1]) < 3) {
+        //     data = changeColorToBlack(data, i);
+        //     data[i] = 255;
+        //     continue;
+        // }
+    }
+
+    console.log(data);
+    return data;
+}
+
+function changeColorToBlack(data, i) { // For removing image feathering
+    for (let j = 1; j < 4; j++) {
+        data[i - j] = 0;
+    }
+
+    return data;
+}
+
+function changeColorToAlpha(data, i) {
+    data[i] = 0;
+    return data;
+}
+
+function checkIfAlphaIndex(index) {
+     return (index % 4) == 0;
+}
+
+function checkIfAlpha(data, i) {
+    return data[i] == 0;
+}
+
+function checkIfWhite(red, green, blue) {
+    return red == 255 && green == 255 && blue == 255;
+}
+
+function checkIfBlack(red, green, blue) {
+    return (red + green + blue) == 0;
+}
+
+function calculateRGBStandardDeviation(red, green, blue) {
+    return Math.sqrt((parseInt(red) + parseInt(green) + parseInt(blue)) / 3);
+}
+
+// function changeWhiteToAlpha(data) {
+//     dataCopy = data; // Create data copy for reference
+//     console.log(data);
+
+//     for (let i = 0; i < data.length; i++) {
+//         if ((i + 1) % 4 == 0 && data[i] > 0 && (dataCopy[i - 1] + dataCopy[i - 2] + dataCopy[i - 3]) > 300 && calculateRGBStandardDeviation(dataCopy[i - 1], dataCopy[i - 2], dataCopy[i - 3]) > 25) {
+//             data[i] = 0;
+//         }
+//     }
+
+//     console.log(data);
+//     return data;
+// }
